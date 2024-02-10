9c878ef11ffb7a1c1789fa7c05d6c4fa7a86f598
himanshudigrase <himanshudigrase@gmail.com>
2024-02-10 18:58:45 +0530

    Template done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['images.pexels.com'], // Add 'images.pexels.com' to the list of allowed domains
+      },
+};
+
+
 
 export default nextConfig;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Navlink from "./Navlink";
-
+import { motion } from "framer-motion";
 
 const links = [
   { url: "/", title: "Home" },
@@ -13,6 +13,58 @@ const links = [
 ];
 const Navbar = () => {
     const [hamOpen, setHamOpen] = useState(false);
+    const topVariants = {
+      closed: {
+        rotate: 0,
+      },
+      opened: {
+        rotate: 45,
+        backgroundColor: "rgb(255,255,255)",
+      },
+    };
+    const centerVariants = {
+      closed: {
+        opacity: 1,
+      },
+      opened: {
+        opacity: 0,
+      },
+    };
+  
+    const bottomVariants = {
+      closed: {
+        rotate: 0,
+      },
+      opened: {
+        rotate: -45,
+        backgroundColor: "rgb(255,255,255)",
+      },
+    };
+
+    const listVariants = {
+      closed: {
+        x: "100vw",
+      },
+      opened: {
+        x: 0,
+        transition: {
+          when: "beforeChildren",
+          staggerChildren: 0.2,
+        },
+      },
+    };
+  
+    const listItemVariants = {
+      closed: {
+        x: -10,
+        opacity: 0,
+      },
+      opened: {
+        x: 0,
+        opacity: 1,
+      },
+    };
+
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
@@ -44,30 +96,33 @@ const Navbar = () => {
           <Image src="/linkedin.png" width={24} height={24}/>
         </Link>
         <Link href="/">
-          <Image src="/github.png" width={24} height={24}/>
+          <Image src="/leetcode.png" width={24} height={24}/>
         </Link>
         <Link href="/">
-          <Image src="/github.png" width={24} height={24}/>
+          <Image src="/email.png" width={28} height={28}/>
         </Link>
       </div>
       <div>
         {/* MENU Button*/}
         <div className="md:hidden">
           <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={()=>setHamOpen(!hamOpen)}>
-            <div className="w-10 h-1 bg-white rounded"></div>
-            <div className="w-10 h-1 bg-white rounded"></div>
-            <div className="w-10 h-1 bg-white rounded"></div>
+            <motion.div variants = {topVariants} animate = {hamOpen? "opened" : "closed"} className="w-10 h-1 bg-white rounded origin-left"></motion.div>
+            <motion.div variants = {centerVariants} animate = {hamOpen? "opened" : "closed"} className="w-10 h-1 bg-white rounded"></motion.div>
+            <motion.div variants = {bottomVariants} animate = {hamOpen? "opened" : "closed"} className="w-10 h-1 bg-white rounded origin-left"></motion.div>
           </button>
           {/* MENU LIST */}
           {
             hamOpen && 
-              <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+              <motion.div variants={listVariants} initial="closed" animate="opened" className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
           {links.map((link) => (
-              <Link href={link.url} key={link.title}>
+              <motion.div variants={listItemVariants} key={link.title} >
+
+            <Link href={link.url}>
               {link.title}
             </Link>
+              </motion.div>
           ))}
-          </div>
+          </motion.div>
          }
          
         </div>
